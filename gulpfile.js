1649d477c1340068659312f3bc33d25344add673
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-08-05 20:50:47 -0700

    gulp watches for scss changes and compiles with watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,53 +4,72 @@ var gulp = require('gulp'),
     ngAnnotate = require('gulp-ng-annotate'),
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
-    gulpAngularProtractor = require('gulp-angular-protractor');
+    gulpAngularProtractor = require('gulp-angular-protractor'),
+    sass = require('gulp-sass');
+
+var paths = {
+  scripts: [
+    'public/javascripts/*.js', //get angular-app
+    'public/javascripts/**/*.js', //get js from feature folders
+    '!public/javascripts/**/*.spec.js', //don't concat/minify protractor tests
+    '!public/javascripts/*.spec.js', //don't concat/minify protractor tests
+    '!public/javascripts/dist/*.js', //ignore previous dist file
+  ],
+  style: 'public/stylesheets/**/*.scss', //get scss to compile to css
+  lint: [
+    'public/javascripts/*.js',
+    'public/javascripts/**/*.js',
+    'public/javascripts/**/*.spec.js', //lint the protractor tests
+    'public/javascripts/*.spec.js', //lint the protractor tests
+    '!public/javascripts/dist/*.js' //ignore previous dist file
+  ],
+  test: '../public/javascripts/**/*.spec.js',
+  watch: [
+    'public/javascripts/*.{js,html}',
+    'public/javascripts/**/*.{js,html}',
+    'public/stylesheets/sass/**/*.scss',
+    '!public/javascripts/dist/*.js'
+  ]
+}
+
+//compiles scss, runs lint on js & tests, then concats & minifies to dist
+gulp.task('scripts', ['style', 'lint'], function() {
+  return gulp
+  .src(paths.scripts)
+  .pipe(concat('all.min.js'))
+  .pipe(ngAnnotate({add: true}))
+  .pipe(uglify({mangle: true}))
+  .pipe(gulp.dest('./public/javascripts/dist'));
+});
+
+//compile scss to css
+gulp.task('style', function() {
+  return gulp
+    .src('./public/stylesheets/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/stylesheets/css/'));
+});
 
 //runs linter on all js and tests
 gulp.task('lint', function() {
   return gulp
-    .src([
-      'public/javascripts/**/*.spec.js',
-      'public/javascripts/*.spec.js',
-    ])
+    .src(paths.lint)
     .pipe(jshint())
     .pipe(jshint.reporter(stylish)) //better styling for linter warnings
-});
-
-//runs lint on js including tests, then concats and minifies to dist
-gulp.task('scripts', ['lint'], function() {
-  return gulp
-    .src([
-      'public/javascripts/*.js', //get angular-app
-      'public/javascripts/**/*.js', //get js from folders organized by section
-      '!public/javascripts/**/*.spec.js', //avoid all protractor test files
-      '!public/javascripts/*.spec.js', //avoid angular-app protractor test file
-      '!public/javascripts/dist/*.js', //ignore previous dist file
-    ])
-    .pipe(concat('all.min.js'))
-    .pipe(ngAnnotate({add: true}))
-    .pipe(uglify({mangle: true}))
-    .pipe(gulp.dest('public/javascripts/dist'));
 });
 
 //runs protractor test
 gulp.task('test', ['scripts'], function(callback) {
   return gulp
-    .src(['../public/javascripts/**/*.spec.js'])
+    .src(paths.test)
     .pipe(gulpAngularProtractor({
       'configFile': 'protractor.conf.js',
       'autoStartStopServer': true,
       'debug': false,
     }))
-    .on('error', function(e) {
-      throw e
-    })
-})
+    .on('error', function(e) {throw e;})
+});
 
 gulp.task('watch', function() {
-  gulp.watch([
-    'public/javascripts/*.{js,html}',
-    'public/javascripts/*/*.{js,html}',
-    '!public/javascripts/dist/*.js'
-  ],['scripts']);
-}) //reruns scripts automatically on changes to these file types
+  gulp.watch(paths.watch, ['scripts']);
+}); //reruns scripts automatically on changes to these file types
